dcd7d04d27da9c2b96817c375f4fbad87e56dae9
Chris Woods <chris.woods@gmail.com>
2021-12-11 22:10:28 -0500

    branches wip

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
 fn main() {
-    let number = 6;
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
 
-    if number % 4 == 0 {
-        println!("number is divisible by 4");
-    } else if number % 3 == 0 {
-        println!("number is divisible by 3");
-    } else if number % 2 == 0 {
-        println!("number is divisble by 2");
-    } else {
-        println!("number is not divisble by 4, 2, or 3");
-    }
+    println!("The value of number is {}", number);
 }
